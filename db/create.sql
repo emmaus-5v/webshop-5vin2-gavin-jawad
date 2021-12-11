d119180866c8efd275660747b0c72df35d57b5cc
112446 <112446@emmauscollege.nl>
2021-12-11 18:53:12 +0000

    yesss
    Signed-off-by: 112446 <112446@emmauscollege.nl>

--- db/create.sql
+++ db/create.sql
@@ -21,10 +21,10 @@ CREATE TABLE products (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price) values ('Volkswagen Golf', 'German besteller', '816905633-0', 34,090.00);
-insert into products (name, description, code, price) values ('Ford Model T', 'American bestseller.', '077030122-3', 24,200.00);
-insert into products (name, description, code, price) values ('Renault Clio', 'French bestseller', 18,569.00);
-insert into products (name, description, code, price) values ('Toyota Corolla', 'Japanese bestseller.', '693155505-7', 27,395.00);
-insert into products (name, description, code, price) values ('Mini', 'British bestseller.', '686928463-6', 27,395.00);
-insert into products (name, description, code, price) values ('Ford Falcon', 'Australian bestseller.', '492662523-7', 21.900,00);
+insert into products (name, description, code, price) values ('Volkswagen Golf', 'German besteller', '816905633-0', 34090.0);
+insert into products (name, description, code, price) values ('Ford Model T', 'American bestseller.', '077030122-3', 24200.0);
+insert into products (name, description, code, price) values ('Renault Clio', 'French bestseller', 18569.0);
+insert into products (name, description, code, price) values ('Toyota Corolla', 'Japanese bestseller.', '693155505-7', 27395.0);
+insert into products (name, description, code, price) values ('Mini', 'British bestseller.', '686928463-6', 27395.0);
+insert into products (name, description, code, price) values ('Ford Falcon', 'Australian bestseller.', '492662523-7', 21900.0);
 
