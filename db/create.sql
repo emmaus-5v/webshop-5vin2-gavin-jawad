b7c6643916de41fc39ef4eba4f0641f31569a93c
112446 <112446@emmauscollege.nl>
2021-12-11 23:29:25 +0000

    yesss
    Signed-off-by: 112446 <112446@emmauscollege.nl>

--- db/create.sql
+++ db/create.sql
@@ -23,7 +23,7 @@ CREATE TABLE products (
 
 insert into products (name, description, code, price) values ('Volkswagen Golf', 'German besteller', '816905633-0', 34090.0);
 insert into products (name, description, code, price) values ('Ford Model T', 'American bestseller.', '077030122-3', 24200.0);
-insert into products (name, description, code, price) values ('Renault Clio', 'French bestseller', 18569.0);
+insert into products (name, description, code, price) values ('Renault Clio', 'French bestseller', '54355232-0', 18569.0);
 insert into products (name, description, code, price) values ('Toyota Corolla', 'Japanese bestseller.', '693155505-7', 27395.0);
 insert into products (name, description, code, price) values ('Mini', 'British bestseller.', '686928463-6', 27395.0);
 insert into products (name, description, code, price) values ('Ford Falcon', 'Australian bestseller.', '492662523-7', 21900.0);
