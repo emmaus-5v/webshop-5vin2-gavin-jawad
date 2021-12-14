3c23570cf87f97faa6d3c6997b8d0775ee7bd3bc
112446 <112446@emmauscollege.nl>
2021-12-14 22:13:42 +0000

    help
    Signed-off-by: 112446 <112446@emmauscollege.nl>

--- db/create.sql
+++ db/create.sql
@@ -2,29 +2,14 @@
 -- create tables
 --
 
-DROP TABLE IF EXISTS products; 
+DROP TABLE IF EXISTS products;
 CREATE TABLE products (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   code VARCHAR(15),
   name VARCHAR(255),
   description TEXT,
   price NUMERIC(10, 2),
-  typen_id INTEGER,
-  merk_id INTEGER,
 );
-
-DROP TABLE IF EXISTS typen; 
-CREATE TABLE typen (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name VARCHAR(25)
-);
-
-DROP TABLE IF EXISTS merk; 
-CREATE TABLE merk (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name VARCHAR(40)
-);
-
 --
 -- populate with data
 --
@@ -34,24 +19,9 @@ CREATE TABLE merk (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price, typen_id, merk_id) values ('Volkswagen Golf', 'German besteller', '816905633-0', 34090.0);
-insert into products (name, description, code, price, typen_id, merk_id) values ('Ford Model T', 'American bestseller.', '077030122-3', 24200.0);
-insert into products (name, description, code, price, typen_id, merk_id) values ('Renault Clio', 'French bestseller', '54355232-0', 18569.0, 4, 5);
-insert into products (name, description, code, price, typen_id, merk_id) values ('Toyota Corolla', 'Japanese bestseller.', '693155505-7', 27395.0);
-insert into products (name, description, code, price, typen_id, merk_id) values ('Mini', 'British bestseller.', '686928463-6', 27395.0);
-insert into products (name, description, code, price, typen_id, merk_id) values ('Ford Falcon', 'Australian bestseller.', '492662523-7', 21900.0);
-
-insert into typen (name) values ('Hatchback');
-insert into typen (name) values ('Sedan');
-insert into typen (name) values ('Station');
-insert into typen (name) values ('Cabriolet');
-insert into typen (name) values ('Coupé');
-insert into typen (name) values ('MVP');
-insert into typen (name) values ('Terreinauto');
-
-insert into merk (name) values ('Toyota');
-insert into merk (name) values ('Volkswagen');
-insert into merk (name) values ('Audi');
-insert into merk (name) values ('Renault');
-insert into merk (name) values ('Ford');
-insert into merk (name) values ('Mini');
+insert into products (name, description, code, price) values ('Volkswagen Golf', 'German besteller', '816905633-0', 34090.0);
+insert into products (name, description, code, price) values ('Ford Model T', 'American bestseller.', '077030122-3', 24200.0);
+insert into products (name, description, code, price) values ('Renault Clio', 'French bestseller', '54355232-0', 18569.0);
+insert into products (name, description, code, price) values ('Toyota Corolla', 'Japanese bestseller.', '693155505-7', 27395.0);
+insert into products (name, description, code, price) values ('Mini', 'British bestseller.', '686928463-6', 27395.0);
+insert into products (name, description, code, price) values ('Ford Falcon', 'Australian bestseller.', '492662523-7', 21900.0);
