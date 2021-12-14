1f0d4a0fea7146cb841b6f53ed5bc86bee35c51d
112446 <112446@emmauscollege.nl>
2021-12-14 21:57:10 +0000

    hhh
    Signed-off-by: 112446 <112446@emmauscollege.nl>

--- db/create.sql
+++ db/create.sql
@@ -8,9 +8,9 @@ CREATE TABLE products (
   code VARCHAR(15),
   name VARCHAR(255),
   description TEXT,
-  price NUMERIC(10, 2)
-  typen_id INTEGER
-  merk_id INTEGER
+  price NUMERIC(10, 2),
+  typen_id INTEGER,
+  merk_id INTEGER,
 );
 
 DROP TABLE IF EXISTS typen; 
@@ -34,24 +34,24 @@ CREATE TABLE merk (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price, typen_id, merk_id) values ('Volkswagen Golf', 'German besteller', '816905633-0', 34090.0, 3, 4);
-insert into products (name, description, code, price, typen_id, merk_id) values ('Ford Model T', 'American bestseller.', '077030122-3', 24200.0, 1, 2);
+insert into products (name, description, code, price, typen_id, merk_id) values ('Volkswagen Golf', 'German besteller', '816905633-0', 34090.0);
+insert into products (name, description, code, price, typen_id, merk_id) values ('Ford Model T', 'American bestseller.', '077030122-3', 24200.0);
 insert into products (name, description, code, price, typen_id, merk_id) values ('Renault Clio', 'French bestseller', '54355232-0', 18569.0, 4, 5);
-insert into products (name, description, code, price, typen_id, merk_id) values ('Toyota Corolla', 'Japanese bestseller.', '693155505-7', 27395.0, 7, 5);
-insert into products (name, description, code, price, typen_id, merk_id) values ('Mini', 'British bestseller.', '686928463-6', 27395.0, 2, 3);
-insert into products (name, description, code, price, typen_id, merk_id) values ('Ford Falcon', 'Australian bestseller.', '492662523-7', 21900.0, 3, 2);
+insert into products (name, description, code, price, typen_id, merk_id) values ('Toyota Corolla', 'Japanese bestseller.', '693155505-7', 27395.0);
+insert into products (name, description, code, price, typen_id, merk_id) values ('Mini', 'British bestseller.', '686928463-6', 27395.0);
+insert into products (name, description, code, price, typen_id, merk_id) values ('Ford Falcon', 'Australian bestseller.', '492662523-7', 21900.0);
 
-insert into typen (name) values ('Hatchback1');
-insert into typen (name) values ('Sedan2');
-insert into typen (name) values ('Station3');
-insert into typen (name) values ('Cabriolet4');
-insert into typen (name) values ('Coupé5');
-insert into typen (name) values ('MVP6');
-insert into typen (name) values ('Terreinauto7');
+insert into typen (name) values ('Hatchback');
+insert into typen (name) values ('Sedan');
+insert into typen (name) values ('Station');
+insert into typen (name) values ('Cabriolet');
+insert into typen (name) values ('Coupé');
+insert into typen (name) values ('MVP');
+insert into typen (name) values ('Terreinauto');
 
-insert into merk (name) values ('Toyota1');
-insert into merk (name) values ('Volkswagen2');
-insert into merk (name) values ('Audi3');
-insert into merk (name) values ('Renault4');
-insert into merk (name) values ('Ford5');
-insert into merk (name) values ('Mini6');
+insert into merk (name) values ('Toyota');
+insert into merk (name) values ('Volkswagen');
+insert into merk (name) values ('Audi');
+insert into merk (name) values ('Renault');
+insert into merk (name) values ('Ford');
+insert into merk (name) values ('Mini');
